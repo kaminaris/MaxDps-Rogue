e569a34e3450c82c521675c650214eeb902137aa
Pawel <zacdbg@gmail.com>
2018-10-21 03:57:16 +0200

    v8.0.0 - BfA Outlaw

--- main.lua
+++ main.lua
@@ -1,280 +1,279 @@
-﻿
+﻿--- @type MaxDps
+if not MaxDps then
+	return ;
+end
+
+local MaxDps = MaxDps;
+local UnitPower = UnitPower;
+local UnitPowerMax = UnitPowerMax;
+local GetPowerRegen = GetPowerRegen;
+local EnumPowerType = Enum.PowerType;
+
+local Rogue = MaxDps:NewModule('Rogue');
+
 -- Outlaw
-local _BladeFlurry = 13877;
-local _RolltheBones = 193316;
-local _TrueBearing = 193359;
-local _SharkInfestedWaters = 193357;
-local _GhostlyStrike = 196937;
-local _CurseoftheDreadblades = 202665;
-local _AdrenalineRush = 13750;
-local _MarkedforDeath = 137619;
-local _RunThrough = 2098;
-local _Broadsides = 193356;
-local _PistolShot = 185763;
-local _Opportunity = 195627;
-local _SaberSlash = 193315;
-local _Vanish = 1856;
-local _Ambush = 8676;
-local _CheapShot = 1833;
-local _PreyontheWeak = 131511;
-local _DeeperStratagem = 193531;
-local _JollyRoger = 199603;
-local _GrandMelee = 193358;
-local _BuriedTreasure = 199600;
-local _SliceandDice = 5171;
-local _DeathfromAbove = 152150;
-local _Vigor = 14983;
-local _CombatPotency = 35551;
-local _Bloodlust = 2825;
-local _Heroism = 32182;
-local _TimeWarp = 80353;
-local _Ruthlessness = 14161;
-local _Sprint = 2983;
-local _BetweentheEyes = 199804;
-local _Blind = 2094;
-local _CloakofShadows = 31224;
-local _Riposte = 199754;
-local _GrapplingHook = 195457;
-local _CannonballBarrage = 185767;
-local _KillingSpree = 51690;
-local _Feint = 1966;
-local _Elusiveness = 79008;
-local _CheatDeath = 31230;
-local _CrimsonVial = 185311;
-local _Stealth = 1784;
-local _HiddenBlade = 202753;
-local _Garrote = 202753;
-local _Kingsbane = 202753;
-local _ToxicBlade = 245388;
-local _Envenom = 202753;
-local _Mutilate = 202753;
+local SO = {
+	Vanish             = 1856,
+	VanishAura         = 11327,
+	Ambush             = 8676,
+	CheapShot          = 1833,
+	PreyOnTheWeak      = 131511,
+	KillingSpree       = 51690,
+	BladeRush          = 271877,
+	BladeFlurry        = 13877,
+	RollTheBones       = 193316,
+	GhostlyStrike      = 196937,
+	AdrenalineRush     = 13750,
+	MarkedForDeath     = 137619,
+	BetweenTheEyes     = 199804,
+	RuthlessPrecision  = 193357,
+	AceUpYourSleeve    = 278676,
+	Deadshot           = 272935,
+	Dispatch           = 2098,
+	PistolShot         = 185763,
+	Opportunity        = 195627,
+	SinisterStrike     = 193315,
+	SnakeEyes          = 275846,
+	MasteryMainGauche  = 76806,
+	SliceAndDice       = 5171,
+	DeeperStratagem    = 193531,
+	TrueBearing        = 193359,
+	SkullAndCrossbones = 199603,
+	GrandMelee         = 193358,
+	Broadside          = 193356,
+	BuriedTreasure     = 199600,
+	LoadedDice         = 256170,
+	Vigor              = 14983,
+	CombatPotency      = 61329,
+	RestlessBlades     = 79096,
+	Ruthlessness       = 14161,
+	GrapplingHook      = 195457,
+	Elusiveness        = 79008,
+	CloakOfShadows     = 31224,
+	Riposte            = 199754,
+	QuickDraw          = 196938,
 
--- Assasination
-local _Rupture = 1943;
-local _Vendetta = 79140;
-local _Vanish = 1856;
-local _Nightstalker = 14062;
-local _Mutilate = 1329;
-local _Garrote = 703;
-local _Envenom = 32645;
-local _Kingsbane = 192759;
-local _FanofKnives = 51723;
-local _AgonizingPoison = 200802;
-local _DeadlyPoison = 2823;
-local _CripplingPoison = 3408;
-local _Vigor = 14983;
-local _VenomousWounds = 79134;
-local _UrgetoKill = 192384;
-local _VenomRush = 152152;
-local _SurgeofToxins = 192424;
-local _SealFate = 14190;
-local _PoisonedKnife = 185565;
-local _DeeperStratagem = 193531;
-local _Exsanguinate = 200806;
-local _MasterAssassin = 192349;
-local _Feint = 1966;
-local _Elusiveness = 79008;
-local _CloakofShadows = 31224;
-local _Evasion = 5277;
-local _CheatDeath = 31230;
-local _CrimsonVial = 185311;
-local _Stealth = 1784;
-local _Berserking = 26297;
+	-- Auras
+	StealthAura        = 1784
+};
 
--- Subtlety
-local _Nightblade = 195452;
-local _ShadowBlades = 121471;
-local _ShadowDance = 185313;
-local _EnvelopingShadows = 206237;
-local _Shadowstrike = 185438;
-local _Anticipation = 114015;
-local _MasterofShadows = 196976;
-local _SymbolsofDeath = 212283;
-local _GoremawsBite = 209783;
-local _Eviscerate = 196819;
-local _Backstab = 53;
+function Rogue:Enable()
+	MaxDps:Print(MaxDps.Colors.Info .. 'Rogue [Outlaw]');
 
--- Auras
-local _Stealth = 1784;
-local _GreenskinsWaterloggedWristcuffs = 209420;
-local _MasterAssassinsInitiative = 235022;
+	if MaxDps.Spec == 1 then
+		MaxDps.NextSpell = Rogue.Assassination;
+	elseif MaxDps.Spec == 2 then
+		MaxDps.NextSpell = Rogue.Outlaw;
+	elseif MaxDps.Spec == 3 then
+		MaxDps.NextSpell = Rogue.Subtlety;
+	end
 
-MaxDps.Rogue = {};
-
-function MaxDps.Rogue.CheckTalents()
+	return true;
 end
 
-function MaxDps:EnableRotationModule(mode)
-	mode = mode or 1;
-	MaxDps.Description = 'Rogue [Outlaw]';
-	MaxDps.ModuleOnEnable = MaxDps.Rogue.CheckTalents;
-	if mode == 1 then
-		MaxDps.NextSpell = MaxDps.Rogue.Assassination;
-	end;
-	if mode == 2 then
-		MaxDps.NextSpell = MaxDps.Rogue.Outlaw;
-	end;
-	if mode == 3 then
-		MaxDps.NextSpell = MaxDps.Rogue.Subtlety;
-	end;
+function Rogue:Assassination(timeShift, currentSpell, gcd, talents, azerite)
+	return nil;
 end
 
-function MaxDps.Rogue.Assassination(_, timeShift, currentSpell, gcd, talents)
+function Rogue:Outlaw(timeShift, currentSpell, gcd, talents, azerite)
 
-	local energy = UnitPower('player', Enum.PowerType.Energy);
-	local combo = UnitPower('player', Enum.PowerType.ComboPoints);
+	local energy = UnitPower('player', EnumPowerType.Energy);
+	local energyMax = UnitPowerMax('player', EnumPowerType.Energy);
+	local energyTimeToMax = (energyMax - energy) / GetPowerRegen();
 
-	local poison = talents[_AgonizingPoison] and _AgonizingPoison or _DeadlyPoison;
+	local combo = UnitPower('player', EnumPowerType.ComboPoints);
+	local comboMax = UnitPowerMax('player', EnumPowerType.ComboPoints);
+	local comboDeficit = comboMax - combo;
 
-	MaxDps:GlowCooldown(_Vendetta, MaxDps:SpellAvailable(_Vendetta, timeShift));
+	local stealthed = MaxDps:Aura(SO.StealthAura, timeShift) or MaxDps:Aura(SO.VanishAura, timeShift);
 
-	if not MaxDps:Aura(poison, timeShift + 60) then
-		return poison;
+	local buffBladeFlurryUp = MaxDps:Aura(SO.BladeFlurry, timeShift);
+	local buffOpportunityUp = MaxDps:Aura(SO.Opportunity, timeShift);
+	local buffAdrenalineRushUp = MaxDps:Aura(SO.AdrenalineRush, timeShift);
+	local _, _, buffSliceAndDiceRemains = MaxDps:Aura(SO.SliceAndDice, timeShift);
+
+	local cooldownBetweenTheEyesReady, cooldownBetweenTheEyesRemains = MaxDps:SpellAvailable(SO.BetweenTheEyes, timeShift);
+	local cooldownGhostlyStrikeReady, cooldownGhostlyStrikeRemains = MaxDps:SpellAvailable(SO.GhostlyStrike, timeShift);
+	local cooldownMarkedForDeathReady, cooldownMarkedForDeathRemains = MaxDps:SpellAvailable(SO.MarkedForDeath, timeShift);
+
+	local buffLoadedDiceUp = MaxDps:Aura(SO.LoadedDice, timeShift);
+	local _, buffSnakeEyesStack = MaxDps:Aura(SO.SnakeEyes, timeShift);
+
+	local Rtb = {
+		Up      = {
+			GrandMelee         = false,
+			RuthlessPrecision  = false,
+			Broadside          = false,
+			BuriedTreasure     = false,
+			TrueBearing        = false,
+			SkullAndCrossbones = false,
+		},
+		Remains = {
+			GrandMelee         = 0,
+			RuthlessPrecision  = 0,
+			Broadside          = 0,
+			BuriedTreasure     = 0,
+			TrueBearing        = 0,
+			SkullAndCrossbones = 0,
+		},
+	};
+	local BuffRollTheBonesUp, BuffRollTheBonesRemains = false, 0;
+	local RtbBuffs = 0;
+	local RtbReroll = false;
+
+	if not talents[SO.SliceAndDice] then
+		Rtb.Up.GrandMelee,         _, Rtb.Remains.GrandMelee         = MaxDps:Aura(SO.GrandMelee, timeShift);
+		Rtb.Up.RuthlessPrecision,  _, Rtb.Remains.RuthlessPrecision  = MaxDps:Aura(SO.RuthlessPrecision, timeShift);
+		Rtb.Up.Broadside,          _, Rtb.Remains.Broadside          = MaxDps:Aura(SO.Broadside, timeShift);
+		Rtb.Up.BuriedTreasure,     _, Rtb.Remains.BuriedTreasure     = MaxDps:Aura(SO.BuriedTreasure, timeShift);
+		Rtb.Up.TrueBearing,        _, Rtb.Remains.TrueBearing        = MaxDps:Aura(SO.TrueBearing, timeShift);
+		Rtb.Up.SkullAndCrossbones, _, Rtb.Remains.SkullAndCrossbones = MaxDps:Aura(SO.SkullAndCrossbones, timeShift);
+
+		for k, v in pairs(Rtb.Remains) do
+			if v > 0 then
+				RtbBuffs = RtbBuffs + 1;
+				BuffRollTheBonesUp = true;
+				BuffRollTheBonesRemains = v;
+			end
+		end
+
+		RtbReroll = RtbBuffs < 2 and
+			(buffLoadedDiceUp or not Rtb.Up.GrandMelee and not Rtb.Up.RuthlessPrecision);
+
+		if azerite[SO.Deadshot] > 0 or azerite[SO.AceUpYourSleeve] > 0 then
+			RtbReroll = RtbBuffs < 2 and
+				(buffLoadedDiceUp or Rtb.Remains.RuthlessPrecision <= cooldownBetweenTheEyesRemains);
+		end
+
+		if azerite[SO.SnakeEyes] > 0 then
+			RtbReroll = RtbBuffs < 2 or (azerite[SO.SnakeEyes] == 3 and RtbBuffs < 5);
+		end
+
+		if azerite[SO.SnakeEyes] >= 2 and buffSnakeEyesStack >= 2 - (Rtb.Up.Broadside and 1 or 0) then
+			RtbReroll = false;
+		end
+
 	end
 
-	if MaxDps:PersistentAura(_Stealth, timeShift) then
-		return _Garrote;
+	local varAmbushCondition = comboDeficit >= 2 + 2 *
+		((talents[SO.GhostlyStrike] and cooldownGhostlyStrikeRemains < 1) and 1 or 0) +
+		((Rtb.Up.Broadside and energy > 60 and not Rtb.Up.SkullAndCrossbones) and 1 or 0);
+	local cpMaxSpend = 5 + (talents[SO.DeeperStratagem] and 1 or 0);
+
+	-- !buff.adrenaline_rush.up&energy.time_to_max>1
+	--if not buffAdrenalineRushUp and energyTimeToMax > 1 then
+	--	return _AdrenalineRush;
+	--end
+
+	-- !stealthed.all&variable.ambush_condition
+	--if not stealthed and varAmbushCondition then
+	--	return _Vanish;
+	--end
+
+	MaxDps:GlowCooldown(
+		SO.AdrenalineRush,
+		MaxDps:SpellAvailable(SO.AdrenalineRush, timeShift) and not buffAdrenalineRushUp and energyTimeToMax > 1
+	);
+	MaxDps:GlowCooldown(
+		SO.Vanish,
+		MaxDps:SpellAvailable(SO.Vanish, timeShift) and not stealthed and varAmbushCondition
+	);
+	if talents[SO.KillingSpree] then
+		MaxDps:GlowCooldown(
+			SO.KillingSpree,
+			MaxDps:SpellAvailable(SO.KillingSpree, timeShift) and (energyTimeToMax > 5 or energy < 15)
+		);
 	end
 
-	if MaxDps:SpellAvailable(_Rupture, timeShift) and combo == 5 and not MaxDps:TargetAura(_Rupture, timeShift + 8) then
-		return _Rupture;
-	end
+	local targets = MaxDps:TargetsInRange(SO.SinisterStrike);
 
-	if MaxDps:SpellAvailable(_Garrote, timeShift) then
-		return _Garrote;
-	end
-
-	if MaxDps:SpellAvailable(_Kingsbane, timeShift) then
-		return _Kingsbane;
-	end
-
-	if MaxDps:SpellAvailable(_ToxicBlade, timeShift) then
-		return _ToxicBlade;
-	end
-
-	if MaxDps:SpellAvailable(_Envenom, timeShift) and combo >=4 and MaxDps:TargetAura(_Rupture, timeShift + 8) then
-		return _Envenom;
-	end
-
-	if MaxDps:SpellAvailable(_Mutilate, timeShift) then
-		return _Mutilate;
-	end
-
-	if talents[_Nightstalker] and combo == 5 then
-		return _Vanish;
-	end
-
-end
-
-function MaxDps.Rogue.Outlaw(_, timeShift, currentSpell, gcd, talents)
-	local energy = UnitPower('player', Enum.PowerType.Energy);
-	local combo = UnitPower('player', Enum.PowerType.ComboPoints);
-
-	MaxDps:GlowCooldown(_AdrenalineRush, MaxDps:SpellAvailable(_AdrenalineRush, timeShift));
-	MaxDps:GlowCooldown(_CurseoftheDreadblades, MaxDps:SpellAvailable(_CurseoftheDreadblades, timeShift));
-	MaxDps:GlowCooldown(_KillingSpree, MaxDps:SpellAvailable(_KillingSpree, timeShift));
-
-	if MaxDps:PersistentAura(_Stealth, timeShift) then
-		return _Ambush;
-	end
-
-	local curse = MaxDps:Aura(_CurseoftheDreadblades, timeShift, 'HARMFUL');
-
-	-- roll the bones auras
-	local rb = {
-		TB = MaxDps:Aura(_TrueBearing, timeShift + 3),
-		SIW = MaxDps:Aura(_SharkInfestedWaters, timeShift + 3),
-		JR = MaxDps:Aura(_JollyRoger, timeShift + 3),
-		GM = MaxDps:Aura(_GrandMelee, timeShift + 3),
-		BS = MaxDps:Aura(_Broadsides, timeShift + 3),
-		BT = MaxDps:Aura(_BuriedTreasure, timeShift + 3),
-	}
-	-- buty, sprint co CD
-	local rbCount = 0;
-	for k, v in pairs(rb) do
-		if v then
-			rbCount = rbCount + 1;
+	-- stealthed.all
+	if stealthed then
+		if energy >= 50 then
+			return SO.Ambush;
 		end
 	end
 
-	local shouldRoll = not rb.TB and rbCount < 2;
-
-	if shouldRoll and combo >= 4 and energy >= 20 then
-		return _RolltheBones;
+	-- raid_event.adds.in>30-raid_event.adds.duration&!stealthed.rogue&combo_points.deficit>=cp_max_spend-1
+	if talents[SO.MarkedForDeath] and MaxDps:SpellAvailable(SO.MarkedForDeath) and not stealthed and
+		comboDeficit >= cpMaxSpend - 1
+	then
+		return SO.MarkedForDeath;
 	end
 
-	if talents[_GhostlyStrike] and not MaxDps:TargetAura(_GhostlyStrike, timeShift + 3) and energy > 27 then
-		return _GhostlyStrike;
+	-- spell_targets>=2&!buff.blade_flurry.up&(!raid_event.adds.exists|raid_event.adds.remains>8|raid_event.adds.in>(2-cooldown.blade_flurry.charges_fractional)*25)
+	if MaxDps:SpellAvailable(SO.BladeFlurry, timeShift) and targets >= 2 and not buffBladeFlurryUp then
+		return SO.BladeFlurry;
 	end
 
-	if talents[_MarkedforDeath] and combo < 2 and MaxDps:SpellAvailable(_MarkedforDeath, timeShift) then
-		return _MarkedforDeath;
+	-- variable.blade_flurry_sync&comboDeficit>=1+buff.broadside.up
+	if talents[SO.GhostlyStrike] and MaxDps:SpellAvailable(SO.GhostlyStrike, timeShift) and energy >= 25 and
+		comboDeficit >= 1 + (Rtb.Up.Broadside and 1 or 0)
+	then
+		return SO.GhostlyStrike;
 	end
 
-	if combo >= 5 and (
-		MaxDps:Aura(_GreenskinsWaterloggedWristcuffs, timeShift) or
-		MaxDps:Aura(_MasterAssassinsInitiative, timeShift)
-	) then
-		return _BetweentheEyes;
+	-- variable.blade_flurry_sync&(energy.time_to_max>5|energy<15)
+	--if talents[SO.KillingSpree] and (energyTimeToMax > 5 or energy < 15) then
+	--	return SO.KillingSpree;
+	--end
+
+	-- nope, on cooldown
+	-- variable.blade_flurry_sync&energy.time_to_max>1
+	if talents[SO.BladeRush] and MaxDps:SpellAvailable(SO.BladeRush, timeShift) and energyTimeToMax > 1
+	then
+		return SO.BladeRush;
 	end
 
-	if (combo >= 6 or (combo >= 5 and rb.BS)) then
-		return _RunThrough;
+
+	-- combo_points>=cp_max_spend-(buff.broadside.up+buff.opportunity.up)*(talent.quick_draw.enabled&(!talent.marked_for_death.enabled|cooldown.marked_for_death.remains>1))
+	if combo >= cpMaxSpend -
+		((Rtb.Up.Broadside and 1 or 0) + (buffOpportunityUp and 1 or 0)) *
+			((talents[SO.QuickDraw] and (not talents[SO.MarkedForDeath] or cooldownMarkedForDeathRemains > 1)) and 1 or 0)
+	then
+
+		-- buff.ruthless_precision.up|(azerite.deadshot.enabled|azerite.ace_up_your_sleeve.enabled)&buff.roll_the_bones.up
+		if cooldownBetweenTheEyesReady and
+			(Rtb.Up.RuthlessPrecision or (azerite[SO.Deadshot] > 0 or azerite[SO.AceUpYourSleeve] > 0) and BuffRollTheBonesUp)
+		then
+			return SO.BetweenTheEyes;
+		end
+
+		-- buff.slice_and_dice.remains<target.time_to_die&buff.slice_and_dice.remains<(1+combo_points)*1.8
+		if talents[SO.SliceAndDice] and buffSliceAndDiceRemains < (1 + combo) * 1.8 then
+			return SO.SliceAndDice;
+		end
+
+		-- (buff.roll_the_bones.remains<=3|variable.rtb_reroll)&(target.time_to_die>20|buff.roll_the_bones.remains<target.time_to_die)
+		if not talents[SO.SliceAndDice] and (BuffRollTheBonesRemains <= 3 or RtbReroll) then
+			return SO.RollTheBones;
+		end
+
+		-- azerite.ace_up_your_sleeve.enabled|azerite.deadshot.enabled
+		if cooldownBetweenTheEyesReady and (azerite[SO.AceUpYourSleeve] > 0 or azerite[SO.Deadshot] > 0) then
+			return SO.BetweenTheEyes;
+		end
+
+		--if energy >= 30 and combo >= 1 then
+			return SO.Dispatch;
+		--end
 	end
 
-	if MaxDps:Aura(_Opportunity, timeShift) and combo <= 4 then
-		return _PistolShot;
+	--
+	-- comboDeficit>=1+buff.broadside.up+talent.quick_draw.enabled&buff.opportunity.up
+	if buffOpportunityUp and
+		comboDeficit >= 1 + (Rtb.Up.Broadside and 1 or 0) + (talents[SO.QuickDraw] and 1 or 0)
+	then
+		return SO.PistolShot;
 	end
 
-	return _SaberSlash;
+	--if energy >= 30 then
+		return SO.SinisterStrike;
+	--end
+
+	--return nil;
 end
 
-function MaxDps.Rogue.Subtlety(_, timeShift, currentSpell, gcd, talents)
-
-	local sdCd, sdCharges = MaxDps:SpellCharges(_ShadowDance, timeShift);
-
-	local isStealth = MaxDps:PersistentAura(_Stealth, timeShift) or MaxDps:PersistentAura(_ShadowDance, timeShift);
-
-	local energy = UnitPower('player', Enum.PowerType.Energy);
-	local combo = UnitPower('player', Enum.PowerType.ComboPoints);
-
-	local generator = _Backstab;
-	if MaxDps:FindSpell(_Shadowstrike) then
-		generator = _Shadowstrike;
-	end
-
-	MaxDps:GlowCooldown(_ShadowDance, sdCd and energy > 50);
-
-	local nbAura, nbAuraCd = MaxDps:TargetAura(_Nightblade, timeShift + 4);
-
-	if combo >= 5 and not nbAura then
-		return _Nightblade;
-	end
-
-	if combo >=5 and nbAura then
-		return _Eviscerate;
-	end
-
-	if MaxDps:SpellAvailable(_ShadowBlades, timeShift) then
-		return _ShadowBlades;
-	end
-
-	if MaxDps:SpellAvailable(_SymbolsofDeath, timeShift) and sdCharges >= 1 and energy <= 60 then
-		return _SymbolsofDeath;
-	end
-
-	if talents[_DeathfromAbove] then
-		return _DeathfromAbove;
-	end
-
-	if MaxDps:SpellAvailable(_GoremawsBite, timeShift) and not isStealth and combo <= 3 and energy <= 50 then
-		return _GoremawsBite;
-	end
-
-	if talents[_DeathfromAbove] and combo >= 5 and MaxDps:Aura(_ShadowDance, timeShift)
-		and MaxDps:Aura(_SymbolsofDeath, timeShift) then
-		return _DeathfromAbove;
-	end
-
-	return generator;
+function Rogue:Subtlety(timeShift, currentSpell, gcd, talents, azerite)
+	return nil;
 end
