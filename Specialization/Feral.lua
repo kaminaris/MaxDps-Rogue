a5b032d8b2d030f19dc45f101037cf25e57f43e0
Pawel <wilkowski@gmail.com>
2019-11-03 03:37:32 +0100

    v8.2.6 - Feral Swipe fix

--- Specialization/Feral.lua
+++ Specialization/Feral.lua
@@ -10,40 +10,6 @@ local UnitPowerMax = UnitPowerMax;
 local Energy = Enum.PowerType.Energy;
 local ComboPoints = Enum.PowerType.ComboPoints;
 
---[[
-local FR = {
-	SavageRoar                 = 52610,
-	Rake                       = 1822,
-	RakeDot                    = 155722,
-	Rip                        = 1079,
-	Sabertooth                 = 202031,
-	FerociousBite              = 22568,
-	LunarInspiration           = 155580,
-	TigersFury                 = 5217,
-	AshamanesFrenzy            = 210722,
-	Shred                      = 5221,
-	Bloodtalons                = 155672,
-	Regrowth                   = 8936,
-	PredatorySwiftness         = 69369,
-	IncarnationKingOfTheJungle = 102543,
-	Prowl                      = 5215,
-	Berserk                    = 106951,
-	Thrash                     = 106830,
-	Swipe                      = 213764,
-	MasteryRazorClaws          = 77493,
-	PrimalFury                 = 159286,
-	JaggedWounds               = 202032,
-	OmenofClarity              = 16864,
-	Predator                   = 202021,
-	ElunesGuidance             = 202060,
-	BrutalSlash                = 202028,
-	ClearCasting               = 135700,
-	FeralFrenzy                = 274837,
-	FeralMoonfire              = 155625,
-	CatForm                    = 768,
-	MassEntanglement           = 102359,
-};
-]]--
 local FR = {
 	Regrowth           = 8936,
 	Bloodtalons        = 155672,
@@ -67,6 +33,7 @@ local FR = {
 	BrutalSlash        = 202028,
 	Thrash             = 106830,
 	ScentOfBlood       = 285564,
+	Swipe2             = 106785,
 	Swipe              = 213764,
 	Moonfire           = 155625,
 	Clearcasting       = 135700,
@@ -248,6 +215,7 @@ function Druid:FeralGenerators()
 	local energy = fd.energy;
 	local Incarnation = fd.Incarnation;
 	local desiredTargets = 2;
+	local Swipe = MaxDps:FindSpell(FR.Swipe) and FR.Swipe or FR.Swipe2;
 
 	-- regrowth,if=talent.bloodtalons.enabled&buff.predatory_swiftness.up&buff.bloodtalons.down&combo_points=4&dot.rake.remains<4;
 	if currentSpell ~= FR.Regrowth and
@@ -287,7 +255,7 @@ function Druid:FeralGenerators()
 
 	-- swipe_cat,if=buff.scent_of_blood.up;
 	if not talents[FR.BrutalSlash] and buff[FR.ScentOfBlood].up then
-		return FR.Swipe;
+		return Swipe;
 	end
 
 	-- rake,target_if=!ticking|(!talent.bloodtalons.enabled&remains<duration*0.3)&target.time_to_die>4;
@@ -335,7 +303,7 @@ function Druid:FeralGenerators()
 
 	-- swipe_cat,if=spell_targets.swipe_cat>1;
 	if not talents[FR.BrutalSlash] and targets > 1 then
-		return FR.Swipe;
+		return Swipe;
 	end
 
 	if talents[FR.BrutalSlash] and cooldown[FR.BrutalSlash].ready and targets > 1 then
@@ -353,68 +321,3 @@ function Druid:FeralGenerators()
 		return FR.Rake;
 	end
 end
-
---[[
-function Druid:Feral()
-	local fd = MaxDps.FrameData;
-	local cooldown, buff, debuff, timeShift, talents, azerite, currentSpell =
-	fd.cooldown, fd.buff, fd.debuff, fd.timeShift, fd.talents, fd.azerite, fd.currentSpell;
-
-	local energy = UnitPower('player', Enum.PowerType.Energy);
-	local combo = UnitPower('player', Enum.PowerType.ComboPoints);
-
-	--Cooldowns
-	local berserk = talents[FR.IncarnationKingOfTheJungle] and FR.IncarnationKingOfTheJungle or FR.Berserk;
-	MaxDps:GlowCooldown(berserk, cooldown[berserk].ready);
-
-	--Dot Aura
-	local ph = MaxDps:TargetPercentHealth();
-
-	-- Rotation
-	if talents[FR.Bloodtalons] and buff[FR.PredatorySwiftness].up and combo >= 4 then --and MaxDps:Aura(FR.SavageRoar, timeShift + 5)
-		return FR.Regrowth;
-	end
-
-	if cooldown[FR.TigersFury].ready and (energy < 30 or buff[berserk].up) then
-		return FR.TigersFury;
-	end
-
-	if talents[FR.FeralFrenzy] and combo == 0 then
-		return FR.FeralFrenzy;
-	end
-
-	local ripPandemic = debuff[FR.Rip].remains < 5;
-
-	if (not debuff[FR.Rip].up and combo >= 5) or
-		(combo >= 5 and not ripPandemic and ph > 0.25 and not talents[FR.Sabertooth])
-	then
-		return FR.Rip;
-	end
-
-	if not ripPandemic and (talents[FR.Sabertooth] or ph < 0.25) and combo >= 5 then
-		return FR.FerociousBite;
-	end
-
-	if talents[FR.SavageRoar] and buff[FR.SavageRoar].remains < 5 and combo >= 5 then
-		return FR.SavageRoar;
-	end
-
-	if combo >= 5 then
-		return FR.FerociousBite;
-	end
-
-	if debuff[FR.RakeDot].remains < 3 then
-		return FR.Rake;
-	end
-
-	if talents[FR.LunarInspiration] and debuff[FR.FeralMoonfire].refreshable then
-		return FR.FeralMoonfire;
-	end
-
-	if talents[FR.BrutalSlash] and cooldown[FR.BrutalSlash].ready and combo < 5 then
-		return FR.BrutalSlash;
-	end
-
-	return FR.Shred;
-end
-]]--
