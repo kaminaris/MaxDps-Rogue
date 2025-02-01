38f66209c947b578e4422006bd5ad2e9b9d8b130
tbkizle <tbkizle@gmail.com>
2025-02-01 02:21:23 -0500

    Classic

---
+++ Specialization/Classic/Combat.lua
@@ -0,0 +1,141 @@
+local _, addonTable = ...
+local Rogue = addonTable.Rogue
+local MaxDps = _G.MaxDps
+if not MaxDps then return end
+local setSpell
+
+local UnitPower = UnitPower
+local UnitHealth = UnitHealth
+local UnitAura = C_UnitAuras.GetAuraDataByIndex
+local UnitAuraByName = C_UnitAuras.GetAuraDataBySpellName
+local UnitHealthMax = UnitHealthMax
+local UnitPowerMax = UnitPowerMax
+local SpellHaste
+local SpellCrit
+local GetSpellInfo = C_Spell.GetSpellInfo
+local GetSpellCooldown = C_Spell.GetSpellCooldown
+local GetSpellCount = C_Spell.GetSpellCastCount
+
+local ManaPT = Enum.PowerType.Mana
+local RagePT = Enum.PowerType.Rage
+local FocusPT = Enum.PowerType.Focus
+local EnergyPT = Enum.PowerType.Energy
+local ComboPointsPT = Enum.PowerType.ComboPoints
+local RunesPT = Enum.PowerType.Runes
+local RunicPowerPT = Enum.PowerType.RunicPower
+local SoulShardsPT = Enum.PowerType.SoulShards
+local LunarPowerPT = Enum.PowerType.LunarPower
+local HolyPowerPT = Enum.PowerType.HolyPower
+local MaelstromPT = Enum.PowerType.Maelstrom
+local ChiPT = Enum.PowerType.Chi
+local InsanityPT = Enum.PowerType.Insanity
+local ArcaneChargesPT = Enum.PowerType.ArcaneCharges
+local FuryPT = Enum.PowerType.Fury
+local PainPT = Enum.PowerType.Pain
+local EssencePT = Enum.PowerType.Essence
+local RuneBloodPT = Enum.PowerType.RuneBlood
+local RuneFrostPT = Enum.PowerType.RuneFrost
+local RuneUnholyPT = Enum.PowerType.RuneUnholy
+
+local fd
+local ttd
+local timeShift
+local gcd
+local cooldown
+local buff
+local debuff
+local talents
+local targets
+local targetHP
+local targetmaxHP
+local targethealthPerc
+local curentHP
+local maxHP
+local healthPerc
+local timeInCombat
+local classtable
+local LibRangeCheck = LibStub('LibRangeCheck-3.0', true)
+
+local Energy
+local EnergyMax
+local EnergyDeficit
+local EnergyRegen
+local EnergyRegenCombined
+local EnergyTimeToMax
+local EnergyPerc
+local ComboPoints
+local ComboPointsMax
+local ComboPointsDeficit
+local PoisonedBleeds
+local DanseMacabreSpellList
+
+local Combat = {}
+
+local function ClearCDs()
+end
+
+function Combat:callaction()
+    if (MaxDps:CheckSpellUsable(classtable.SinisterStrike, 'Sinister Strike')) and (ComboPoints < 5 and talents[classtable.SwordSpecialization] or not talents[classtable.DaggerSpecialization]) and cooldown[classtable.SinisterStrike].ready then
+        if not setSpell then setSpell = classtable.SinisterStrike end
+    end
+    if (MaxDps:CheckSpellUsable(classtable.Backstab, 'Backstab')) and ((UnitThreatSituation("player") == 0) and ComboPoints < 5 and talents[classtable.DaggerSpecialization]) and cooldown[classtable.Backstab].ready then
+        if not setSpell then setSpell = classtable.Backstab end
+    end
+    if (MaxDps:CheckSpellUsable(classtable.SliceandDice, 'Slice and Dice')) and (ComboPoints >= 5 and not buff[classtable.SliceandDice].up or buff[classtable.SliceandDice].remains <= gcd) and cooldown[classtable.SliceandDice].ready then
+        if not setSpell then setSpell = classtable.SliceandDice end
+    end
+    if (MaxDps:CheckSpellUsable(classtable.Eviscerate, 'Eviscerate')) and (ComboPoints >= 5) and cooldown[classtable.Eviscerate].ready then
+        if not setSpell then setSpell = classtable.Eviscerate end
+    end
+end
+function Rogue:Combat()
+    fd = MaxDps.FrameData
+    ttd = (fd.timeToDie and fd.timeToDie) or 500
+    timeShift = fd.timeShift
+    gcd = fd.gcd
+    cooldown = fd.cooldown
+    buff = fd.buff
+    debuff = fd.debuff
+    talents = fd.talents
+    targets = MaxDps:SmartAoe()
+    Mana = UnitPower('player', ManaPT)
+    ManaMax = UnitPowerMax('player', ManaPT)
+    ManaDeficit = ManaMax - Mana
+    targetHP = UnitHealth('target')
+    targetmaxHP = UnitHealthMax('target')
+    targethealthPerc = (targetHP >0 and targetmaxHP >0 and (targetHP / targetmaxHP) * 100) or 100
+    curentHP = UnitHealth('player')
+    maxHP = UnitHealthMax('player')
+    healthPerc = (curentHP / maxHP) * 100
+    timeInCombat = MaxDps.combatTime or 0
+    classtable = MaxDps.SpellTable
+    SpellHaste = UnitSpellHaste('player')
+    SpellCrit = GetCritChance()
+    Energy = UnitPower('player', EnergyPT)
+    EnergyMax = UnitPowerMax('player', EnergyPT)
+    EnergyDeficit = EnergyMax - Energy
+    EnergyRegen = GetPowerRegenForPowerType(Enum.PowerType.Energy)
+    EnergyTimeToMax = EnergyDeficit / EnergyRegen
+    EnergyPerc = (Energy / EnergyMax) * 100
+    ComboPoints = UnitPower('player', ComboPointsPT)
+    ComboPointsMax = UnitPowerMax('player', ComboPointsPT)
+    ComboPointsDeficit = ComboPointsMax - ComboPoints
+
+    classtable.SwordSpecialization = 12815
+    classtable.DaggerSpecialization = 13807
+    classtable.Stealth = 1787
+
+    local function debugg()
+    end
+
+
+    --if MaxDps.db.global.debugMode then
+    --   debugg()
+    --end
+
+    setSpell = nil
+    ClearCDs()
+
+    Combat:callaction()
+    if setSpell then return setSpell end
+end
---
+++ Specialization/Classic/Subtlety.lua
@@ -0,0 +1,147 @@
+local _, addonTable = ...
+local Rogue = addonTable.Rogue
+local MaxDps = _G.MaxDps
+if not MaxDps then return end
+local setSpell
+
+local UnitPower = UnitPower
+local UnitHealth = UnitHealth
+local UnitAura = C_UnitAuras.GetAuraDataByIndex
+local UnitAuraByName = C_UnitAuras.GetAuraDataBySpellName
+local UnitHealthMax = UnitHealthMax
+local UnitPowerMax = UnitPowerMax
+local SpellHaste
+local SpellCrit
+local GetSpellInfo = C_Spell.GetSpellInfo
+local GetSpellCooldown = C_Spell.GetSpellCooldown
+local GetSpellCount = C_Spell.GetSpellCastCount
+
+local ManaPT = Enum.PowerType.Mana
+local RagePT = Enum.PowerType.Rage
+local FocusPT = Enum.PowerType.Focus
+local EnergyPT = Enum.PowerType.Energy
+local ComboPointsPT = Enum.PowerType.ComboPoints
+local RunesPT = Enum.PowerType.Runes
+local RunicPowerPT = Enum.PowerType.RunicPower
+local SoulShardsPT = Enum.PowerType.SoulShards
+local LunarPowerPT = Enum.PowerType.LunarPower
+local HolyPowerPT = Enum.PowerType.HolyPower
+local MaelstromPT = Enum.PowerType.Maelstrom
+local ChiPT = Enum.PowerType.Chi
+local InsanityPT = Enum.PowerType.Insanity
+local ArcaneChargesPT = Enum.PowerType.ArcaneCharges
+local FuryPT = Enum.PowerType.Fury
+local PainPT = Enum.PowerType.Pain
+local EssencePT = Enum.PowerType.Essence
+local RuneBloodPT = Enum.PowerType.RuneBlood
+local RuneFrostPT = Enum.PowerType.RuneFrost
+local RuneUnholyPT = Enum.PowerType.RuneUnholy
+
+local fd
+local ttd
+local timeShift
+local gcd
+local cooldown
+local buff
+local debuff
+local talents
+local targets
+local targetHP
+local targetmaxHP
+local targethealthPerc
+local curentHP
+local maxHP
+local healthPerc
+local timeInCombat
+local classtable
+local LibRangeCheck = LibStub('LibRangeCheck-3.0', true)
+
+local Energy
+local EnergyMax
+local EnergyDeficit
+local EnergyRegen
+local EnergyRegenCombined
+local EnergyTimeToMax
+local EnergyPerc
+local ComboPoints
+local ComboPointsMax
+local ComboPointsDeficit
+local PoisonedBleeds
+local DanseMacabreSpellList
+
+local Subtlety = {}
+
+local function ClearCDs()
+end
+
+function Subtlety:callaction()
+    if (MaxDps:CheckSpellUsable(classtable.Ambush, 'Ambush')) and (buff[classtable.Stealth].up) and cooldown[classtable.Ambush].ready then
+        if not setSpell then setSpell = classtable.Ambush end
+    end
+    if (MaxDps:CheckSpellUsable(classtable.Hemorrhage, 'Hemorrhage')) and (ComboPoints < 5 and not debuff[classtable.Hemorrhage].up or debuff[classtable.Hemorrhage].refreshable) and cooldown[classtable.Hemorrhage].ready then
+        if not setSpell then setSpell = classtable.Hemorrhage end
+    end
+    if (MaxDps:CheckSpellUsable(classtable.GhostlyStrike, 'Ghostly Strike')) and (ComboPoints < 5) and cooldown[classtable.GhostlyStrike].ready then
+        if not setSpell then setSpell = classtable.GhostlyStrike end
+    end
+    if (MaxDps:CheckSpellUsable(classtable.SliceandDice, 'Slice and Dice')) and (ComboPoints >= 5 and not buff[classtable.SliceandDice].up or buff[classtable.SliceandDice].remains <= gcd) and cooldown[classtable.SliceandDice].ready then
+        if not setSpell then setSpell = classtable.SliceandDice end
+    end
+    if (MaxDps:CheckSpellUsable(classtable.Eviscerate, 'Eviscerate')) and (ComboPoints >= 5) and cooldown[classtable.Eviscerate].ready then
+        if not setSpell then setSpell = classtable.Eviscerate end
+    end
+end
+function Rogue:Subtlety()
+    fd = MaxDps.FrameData
+    ttd = (fd.timeToDie and fd.timeToDie) or 500
+    timeShift = fd.timeShift
+    gcd = fd.gcd
+    cooldown = fd.cooldown
+    buff = fd.buff
+    debuff = fd.debuff
+    talents = fd.talents
+    targets = MaxDps:SmartAoe()
+    Mana = UnitPower('player', ManaPT)
+    ManaMax = UnitPowerMax('player', ManaPT)
+    ManaDeficit = ManaMax - Mana
+    targetHP = UnitHealth('target')
+    targetmaxHP = UnitHealthMax('target')
+    targethealthPerc = (targetHP >0 and targetmaxHP >0 and (targetHP / targetmaxHP) * 100) or 100
+    curentHP = UnitHealth('player')
+    maxHP = UnitHealthMax('player')
+    healthPerc = (curentHP / maxHP) * 100
+    timeInCombat = MaxDps.combatTime or 0
+    classtable = MaxDps.SpellTable
+    SpellHaste = UnitSpellHaste('player')
+    SpellCrit = GetCritChance()
+    Energy = UnitPower('player', EnergyPT)
+    EnergyMax = UnitPowerMax('player', EnergyPT)
+    EnergyDeficit = EnergyMax - Energy
+    EnergyRegen = GetPowerRegenForPowerType(Enum.PowerType.Energy)
+    EnergyTimeToMax = EnergyDeficit / EnergyRegen
+    EnergyPerc = (Energy / EnergyMax) * 100
+    ComboPoints = UnitPower('player', ComboPointsPT)
+    ComboPointsMax = UnitPowerMax('player', ComboPointsPT)
+    ComboPointsDeficit = ComboPointsMax - ComboPoints
+
+    classtable.Ambush = 11269
+    classtable.Stealth = 1787
+    classtable.Hemorrhage = 17348
+    classtable.GhostlyStrike = 14278
+    classtable.SliceandDice = 6774
+    classtable.Eviscerate = 31016
+
+    local function debugg()
+    end
+
+
+    --if MaxDps.db.global.debugMode then
+    --   debugg()
+    --end
+
+    setSpell = nil
+    ClearCDs()
+
+    Subtlety:callaction()
+    if setSpell then return setSpell end
+end
